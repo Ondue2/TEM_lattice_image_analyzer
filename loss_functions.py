cfff454c2485057bfe714213de8dd7fbafe137b2
Sang-Eon Lee <transcendate1@gmail.com>
2026-01-27 17:24:01 -0500

    Update loss_functions.py

--- loss_functions.py
+++ loss_functions.py
@@ -6,6 +6,7 @@ Created on Mon Oct 28 17:57:27 2024
 """
 
 import tensorflow as tf
+import numpy as np
 
 def compute_loss_tf(given_image, simulated_image):
 
@@ -13,3 +14,13 @@ def compute_loss_tf(given_image, simulated_image):
     count_num = tf.math.count_nonzero(simulated_image, dtype=tf.float32)
    
     return fit_loss/count_num
+
+def compute_loss_np(given_image, simulated_image):
+
+    fit_loss = np.sum(np.where(simulated_image != 0, np.square(given_image - simulated_image), 0))
+    count_num = np.count_nonzero(simulated_image)
+
+    loss = fit_loss/count_num 
+
+    return loss
+
